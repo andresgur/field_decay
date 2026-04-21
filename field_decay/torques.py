45287edf10abbf4a20e006249933c63cd00cdef6
andresgur <andress.gurpide@gmail.com>
2026-04-21 18:50:55 +0200

    Added chi dependencies of the torques, as these are independent of projection effects

--- field_decay/torques.py
+++ field_decay/torques.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import cos, pi, sin
 from numba import jit, njit, float64
 from accretion import M_NS_default
 from constants import Gcgs, ccgs
@@ -24,7 +24,9 @@ def accretion_torque(Mdot: float, Rmag: float, M_NS: float = M_NS_default) -> fl
 
 
 @njit
-def torque_wang(Mdot: float, Rmag: float, omega: float, M_NS=M_NS_default):
+def torque_wang(
+    Mdot: float, Rmag: float, omega: float, M_NS=M_NS_default, chi: float = 0
+):
     """Computes the accretion torque onto a NS according to Wang+95 (actually taken from Vasilopoulos+2018).
     Equation 19 from Wang+95
     All units in cgs
@@ -47,7 +49,7 @@ def torque_wang(Mdot: float, Rmag: float, omega: float, M_NS=M_NS_default):
 
 # if there are defaults do not type the function with jit, as it does not work with kwargs. We can use njit instead, but we cannot use the default value for M_NS (we can set it to 1.4 M_sun in grams, but it is not as clear)
 @njit
-def magnetic_torque_wang(Mdot, Rmag, omega, M_NS=M_NS_default):
+def magnetic_torque_wang(Mdot, Rmag, omega, M_NS=M_NS_default, chi=0.0):
     """This is like the above, but only considering the magnetic term.
     It is a spin down term due to magnetic field lines threading the disc beyond the co-rotation radius
 
@@ -61,9 +63,17 @@ def magnetic_torque_wang(Mdot, Rmag, omega, M_NS=M_NS_default):
         Fastness parameter
     M_NS: float
         Mass of the NS, defaults to 1.4 solar massess (in g)
+    chi:float
+        Magnetic obliquity angle in radians. Here the term cos(chi)**2 comes from the Bz component and the Bphi \propto Bz,
+        which results in a cos(chi)**2 dependence. See Wang 1997
     """
     N_0 = accretion_torque(Mdot, Rmag, M_NS)
-    return N_0 / (1.0 - omega) * (1.0 / 6.0 - omega / 3.0 + omega**2.0 / 9)
+    return (
+        N_0
+        / (1.0 - omega)
+        * (1.0 / 6.0 - omega / 3.0 + omega**2.0 / 9)
+        * cos(chi) ** 2.0
+    )
 
 
 @njit
@@ -99,7 +109,9 @@ def accretion_torque_dai(
 
 
 @njit
-def magnetic_torque_dai(mu: float, Rmag: float, omega: float, gamma=1) -> float:
+def magnetic_torque_dai(
+    mu: float, Rmag: float, omega: float, gamma=1, chi: float = 0.0
+) -> float:
     """Computes the magnetic torque onto a NS (valid during accretion) i.e. for w >=1 (Lai & Li 2006)
         Their Equation 7
 
@@ -113,9 +125,12 @@ def magnetic_torque_dai(mu: float, Rmag: float, omega: float, gamma=1) -> float:
         Fastness parameter
     gamma: float,
         Factor to account from Equation 4
+    chi: float,
+        Magnetic obliquity angle in radians. Here the term cos(chi)**2 comes from the Bz component and the Bphi \propto Bz,
+        which results in a cos(chi)**2 dependence.
     """
     factor = gamma * mu**2.0 / (3.0 * Rmag**3)
-    Nmag = factor * (1.0 - 2.0 * omega + 2.0 * omega**2.0 / 3.0)
+    Nmag = factor * (1.0 - 2.0 * omega + 2.0 * omega**2.0 / 3.0) * cos(chi) ** 2.0
     return Nmag
 
 
@@ -126,22 +141,26 @@ def magnetic_torque_radial_twisting(
     omega: float,
     gamma: float = 1,
     h_0: float = 0.1,
+    chi: float = 0.0,
 ) -> float:
     """Computes the torque due to radial shearing at R0 from Wang 1997 (Equation 8, but without the accretion and threaded disc contribution)
 
     h_0: float,
         The aspect ratio of the disk at Rmag (H/R).
 
+    chi: float,
+        Magnetic obliquity angle in radians. Here the term sin(chi)**2 comes from the Br component and the Bphi \propto Br, which results in a sin*(chi)**2 dependence.
+
 
     """
     factor = 2 * gamma * mu**2.0 / (Rmag**3.0)
-    Nmag = factor * (1 - omega) * h_0
+    Nmag = factor * (1 - omega) * h_0 * sin(chi) ** 2.0
     return Nmag
 
 
 @njit
 def magnetic_torque_dai_propeller(
-    mu: float, Rmag: float, omega: float, gamma: float = 1
+    mu: float, Rmag: float, omega: float, gamma: float = 1, chi: float = 0.0
 ):
     """Computes the magnetic torque onto a NS (valid during propeller i.e. for w <1) (Lai & Li 2006)
     Their Equation 7
@@ -156,9 +175,12 @@ def magnetic_torque_dai_propeller(
         Fastness parameter
     gamma: float,
         Factor to account from Equation 4
+    chi: float,
+        Magnetic obliquity angle in radians. Here the term cos(chi)**2 comes from the Bz component and the Bphi \propto Bz,
+        which results in a cos(chi)**2 dependence.
     """
     factor = gamma * mu**2.0 / (3.0 * Rmag**3.0)
-    Nmag = factor * (2.0 / (3.0 * omega) - 1.0)
+    Nmag = factor * (2.0 / (3.0 * omega) - 1.0) * cos(chi) ** 2.0
     return Nmag
 
 
